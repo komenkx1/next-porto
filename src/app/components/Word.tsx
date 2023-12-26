b8d155b0eedd4e1ac655e8dd07339058947e4295
I komang wahyu hadi permana <44283571+komenkx1@users.noreply.github.com>
2023-12-26 14:04:26 +0800

    fix responsive word section

--- src/app/components/Word.tsx
+++ src/app/components/Word.tsx
@@ -4,31 +4,34 @@ export default function Word() {
       <div className="title w-full text-center mb-10">
         <h3
           className="
-        text-gray-200
-        text-[40px]
-        font-bold
-        font-['Space
-        Grotesk']
-        leading-[48px]
-        mb-5
+          text-gray-200
+            lg:text-[40px]
+            md:text-[30px]
+            text-2xl
+            font-bold
+            leading-[48px]
+            lg:mb-5
+            md:mb-3
+            mb--2
         "
         >
           Kind Words
         </h3>
         <span
           className="text-gray-300
-        text-xl
+        lg:text-xl
         font-normal
-        font-['Space
-        Grotesk']
+        lg:text-start md:text-lg text-normal
         leading-7"
-        >See what my clients have to say about working with me</span>
+        >
+          See what my clients have to say about working with me
+        </span>
       </div>
-      <div className="lg:grid  grid-rows-1 grid-flow-col gap-4">
+      <div className="lg:grid grid-rows-1 grid-flow-col gap-4">
         <div className="col-span-2 first flex flex-col">
           <div className="flex-1 pl-10 pr-[23px] py-10 my-5 bg-gray-800 bg-opacity-40 rounded-xl border border-gray-500 backdrop-blur-[20px] flex-col justify-start items-start gap-2 inline-flex">
             <div className="flex-col justify-start items-start gap-9 flex">
-              <div className=" text-gray-300 text-xl font-normal font-['Space Grotesk'] leading-7">
+              <div className=" text-gray-300 lg:text-xl md:text-lg text-normal font-normal font-['Space Grotesk'] leading-7">
                 His quality of work is excellent! I prefer Mehedi in every
                 important work of design. He is working fast, clean and present
                 great usability results.
@@ -40,7 +43,7 @@ export default function Word() {
           </div>
           <div className="flex-1 pl-10 pr-[23px] py-10 my-5 bg-gray-800 bg-opacity-40 rounded-xl border border-gray-500 backdrop-blur-[20px] flex-col justify-start items-start gap-2 inline-flex">
             <div className="flex-col justify-start items-start gap-9 flex">
-              <div className=" text-gray-300 text-xl font-normal font-['Space Grotesk'] leading-7">
+              <div className=" text-gray-300 lg:text-xl md:text-lg text-normal font-normal font-['Space Grotesk'] leading-7">
                 His quality of work is excellent! I prefer Mehedi in every
                 important work of design. He is working fast, clean and present
                 great usability results.
@@ -54,7 +57,7 @@ export default function Word() {
         <div className="row-span-2 second flex flex-1">
           <div className="pl-10 pr-[23px] py-10 my-5 bg-gray-800 bg-opacity-40 rounded-xl border border-gray-500 backdrop-blur-[20px] flex-col justify-start items-start gap-2 inline-flex">
             <div className="flex-col justify-start items-start gap-9 flex">
-              <div className=" text-gray-300 text-xl font-normal font-['Space Grotesk'] leading-7">
+              <div className=" text-gray-300 lg:text-xl md:text-lg text-normal font-normal font-['Space Grotesk'] leading-7">
                 His quality of work is excellent! I prefer Mehedi in every
                 important work of design. He is working fast, clean and present
                 great usability results.
