8b9a92a11e6da67f1791be367d09a22570ee7f4d
I komang wahyu hadi permana <44283571+komenkx1@users.noreply.github.com>
2023-12-27 09:19:46 +0800

    add search bar portofolio

--- src/app/components/Portofolio.tsx
+++ src/app/components/Portofolio.tsx
@@ -7,15 +7,17 @@ import {
   MagnifyingGlassCircleIcon,
   MagnifyingGlassIcon,
   PhoneIcon,
+  XMarkIcon,
 } from "@heroicons/react/16/solid";
 import { useCategoryStore } from "../store/categories";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import PortoFolioCard from "./PortofolioCard";
 import HeaderSection from "./HeaderSection";
 import Button from "./Button";
 import { usePortofolioStore } from "../store/portofolio";
 
 export default function Portofolio() {
+  const [isSearch, setSearch] = useState(false);
   const { categories: categories } = useCategoryStore();
   const { portofolio: portofolio, loadMorePortofolio: loadMorePortofolio } =
     usePortofolioStore();
@@ -78,6 +80,7 @@ export default function Portofolio() {
   const futureCategory = useMemo(() => {
     return categories.filter((category) => category.isFutured === true);
   }, [categories]);
+
   return (
     <>
       <div className="section ttile" id="portofolio">
@@ -90,7 +93,15 @@ export default function Portofolio() {
             {futureCategory.map((category, index) => {
               const isLastIndex: boolean = index === futureCategory.length - 1;
               return (
-                <span key={index} data-aos="fade-right" className="flex gap-2 items-center">
+                <span
+                  key={index}
+                  data-aos="fade-right"
+                  className={`!transition-all flex gap-2 items-center ${
+                    isSearch
+                      ? "invisible absolute"
+                      : "block aos-init aos-animate"
+                  }`}
+                >
                   {!isLastIndex ? (
                     <GlobeAltIcon className="w-5 h-5" color="#EAB308" />
                   ) : (
@@ -103,10 +114,42 @@ export default function Portofolio() {
                 </span>
               );
             })}
-            <span data-aos="fade-right" className="!transition-all hover:bg-white cursor-pointer hover:p-2 rounded-md hover:text-black flex gap-2 items-center">
+            <span
+              onClick={() => {
+                setSearch((prevState) => !prevState);
+              }}
+              data-aos="fade-right"
+              className={`!transition-all hover:bg-white cursor-pointer hover:p-2 rounded-md hover:text-black flex gap-2 items-center ${
+                isSearch ? "invisible absolute" : "block aos-init aos-animate"
+              }`}
+            >
               <MagnifyingGlassIcon className="w-5 h-5" color="#3B82F6" />
               Search
             </span>
+            <div
+              data-aos="fade-left"
+              className={`${
+                !isSearch ? "invisible absolute" : "flex items-center gap-2 aos-init aos-animate"
+              }`}
+            >
+              <input
+                type="text"
+                placeholder="Search Item"
+                className={`text-black w-full p-3 rounded-lg focus:outline-2 focus:outline-purple-500 ${
+                  !isSearch
+                    ? "invisible absolute"
+                    : "block aos-init aos-animate"
+                }`}
+              />
+              <div
+                onClick={() => {
+                  setSearch((prevState) => !prevState);
+                }}
+                className={`!transition-all bg-white cursor-pointer p-2 rounded-md text-black flex gap-2 items-center`}
+              >
+                <XMarkIcon className="w-5 h-5" color="black" />
+              </div>
+            </div>
           </HeaderSection>
         </div>
         <div
