5f39114f0b3e9ebd172e142f3473c4a41cc771ba
I komang wahyu hadi permana <44283571+komenkx1@users.noreply.github.com>
2024-01-07 06:06:06 +0800

    try to test update cors

--- next.config.js
+++ next.config.js
@@ -1,12 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['via.placeholder.com','res.cloudinary.com'],
+        domains: ['via.placeholder.com', 'res.cloudinary.com'],
     },
     // use @ alias for src folder
     webpack: (config) => {
         config.resolve.alias['@'] = __dirname
         return config
+    },
+    //fix cors
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                    },
+                ],
+            },
+        ]
     },
 }
 
