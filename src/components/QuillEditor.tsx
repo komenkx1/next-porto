f50b5606ccdffacde418c627ecf9e216cc5cae83
I komang wahyu hadi permana <44283571+komenkx1@users.noreply.github.com>
2024-01-07 13:54:52 +0800

    try to fix quill ediotr failed build

--- src/components/QuillEditor.tsx
+++ src/components/QuillEditor.tsx
@@ -1,6 +1,11 @@
-import ReactQuill from "react-quill";
+import dynamic from 'next/dynamic';
 import "react-quill/dist/quill.snow.css";
-
+const ReactQuill = dynamic(
+	() => {
+		return import('react-quill');
+	},
+	{ ssr: false }
+);
 interface QuillEditorProps {
   value: string;
   onChange: (value: string) => void;
